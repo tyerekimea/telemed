e40dd452b91c0c0f81d5acea38ef37002e56f6c9
tyerekimea <tyerekimea@gmail.com>
2026-08-05 23:52:31 +0000

    update

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/call/page.js -> @daily-co/daily-js\":{\"id\":\"app/call/page.js -> @daily-co/daily-js\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_daily-co_daily-js_dist_daily-esm_js.js\"]}}"
